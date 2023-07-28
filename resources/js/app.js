9a5afa5a96f78d671cde78a59e57ca4d62a4395c
ariful-dev <ariful0027@gmail.com>
2023-07-28 23:55:29 +0600

    test commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,21 @@ import 'admin-lte/dist/js/adminlte.min.js';
 import {createApp} from 'vue';
 import App from './layouts/App.vue';
 import App2 from './layouts/App2.vue';
+import  Login from './pages/auth/Login.vue';
 
+// import Vue from 'vue';
+// import createApp from 'vue';
+// import { createRouter, createWebHistory } from 'vue-router';
 import Router from './routes.js';
 
+// const app = Vue({});
+
+// const router = createRouter({
+
+//     routes: Routes,
+//     history: createWebHistory()
+
+// });
 
 createApp(App)
 .use(Router)
